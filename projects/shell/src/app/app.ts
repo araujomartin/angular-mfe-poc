db5b4421a136eda90c42377312c74e2223c967c8
Martin Araujo <martinaraujo484@gmail.com>
2025-07-23 21:09:55 -0300

    style: shell app

--- projects/shell/src/app/app.ts
+++ projects/shell/src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+declare const require: any;
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class App {
   protected readonly title = signal('shell');
+  protected readonly angularVersion = require('../../../../package.json').dependencies['@angular/core'];
 }
