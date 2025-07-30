a2fe67cfdb4270ec25aa4b74b3e9168ccbb377b5
Martin Araujo <martinaraujo484@gmail.com>
2025-07-30 14:56:55 -0300

    style: change style from mfe-1 app

--- projects/mfe-1/src/app/app.ts
+++ projects/mfe-1/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+declare const require: any;
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('mfe-1');
+  protected readonly angularVersion = require('../../../../package.json').dependencies['@angular/core'];
 }
