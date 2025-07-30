8ad99e7a236586bd42453b0fb69126a6bf485ff9
Martin Araujo <martinaraujo484@gmail.com>
2025-07-30 10:16:05 -0300

    feat: add @angular-architects/native-federation and config

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { loadRemoteModule as loadNativeFederationRemote } from '@angular-architects/native-federation';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: 'mfe-1',
+        loadComponent: () => loadNativeFederationRemote('mfe-1', './Component').then(m => m.App),
+    },
+    {
+        path: '**',
+        redirectTo: 'mfe-1'
+    }
+];
